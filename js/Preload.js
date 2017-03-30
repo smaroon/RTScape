83aeeba867511a8a65265bd0912d4a6fec9e02db
Jeanine Kimball <jeanine.kimball@coxautoinc.com>
2017-03-30 13:04:47 -0400

    Splash and cloud
    Added splash screen with play button and added clouds to game

--- js/Preload.js
+++ js/Preload.js
@@ -10,19 +10,24 @@ Runner.Preload.prototype = {
         this.load.spritesheet('mh', 'assets/images/mh.png', 132, 132);
         this.load.spritesheet('mv', 'assets/images/mv.png', 132, 132);
         this.load.spritesheet('mv2', 'assets/images/mv2.png', 132, 132);
-        this.load.spritesheet('er1', 'assets/images/er.png', 132, 115); // todo create non santa version
+        this.load.spritesheet('er2', 'assets/images/er2.png', 132, 115); // todo create non santa version
         this.load.spritesheet('jm', 'assets/images/jm.png', 132,132);
         //todo: add JM && ER
+
+        this.load.image('splash', 'assets/images/RTScape!.png');
+        this.load.image('playButton', 'assets/images/playbutton.png');
 
         this.load.image('floor', 'assets/images/floor.png');
         this.load.image('monitor', 'assets/images/monitors.png');
         this.load.image('background', 'assets/images/background.png');
+
+        this.load.spritesheet('cloud', 'assets/images/aws1.png', 480, 55);
 
         this.game.stage.backgroundColor = "#8f9fba";
 
         this.game.load.audio('music', ['assets/audio/The Final Countdown.wav'], true);
     },
     create: function() {
-        this.state.start('Game');
+        this.state.start('Splash');
     }
 };
